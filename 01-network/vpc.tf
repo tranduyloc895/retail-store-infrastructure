99558252e74f8aa89180052b6af6f06906a3e554
Duy Lộc Trần <tranduyloc895@gmail.com>
2026-03-16 15:09:27 +0700

    Init

---
+++ 01-network/vpc.tf
@@ -0,0 +1,26 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.5.0"
+
+  name = "ecommerce-vpc"
+  cidr = "10.0.0.0/16" 
+
+  azs             = ["ap-southeast-1a", "ap-southeast-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  # Cấu hình Routing & NAT Gateway
+  enable_nat_gateway   = true
+  single_nat_gateway   = true  
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  # EKS Subnet Tags
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = "1" 
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = "1" 
+  }
+}
